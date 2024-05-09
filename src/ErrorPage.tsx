8f42ebdb9ac6f3427742eacb9df0571f8e731b0e
Jideotetic <jideotetic@gmail.com>
2024-05-09 07:40:41 +0100

    Implement the error route

---
+++ src/ErrorPage.tsx
@@ -0,0 +1,32 @@
+import { useRouteError, isRouteErrorResponse, Link } from "react-router-dom";
+
+export default function ErrorPage() {
+  const error = useRouteError();
+  let errorMessage;
+  let errorCode;
+
+  if (isRouteErrorResponse(error)) {
+    errorCode = error.status;
+    errorMessage = error.data?.message || error.statusText;
+  } else if (error instanceof Error) {
+    errorMessage = error.message;
+  } else if (typeof error === "string") {
+    errorMessage = error;
+  } else {
+    errorMessage = "Unknown error";
+  }
+
+  return (
+    <>
+      <div className="flex h-screen flex-col justify-center gap-4 bg-white text-center">
+        <p className="text-5xl font-bold">{errorCode}</p>
+        <p>{errorMessage}</p>
+        <p>
+          <Link to="/" className="text-[#E87407] underline hover:no-underline">
+            Go Home
+          </Link>
+        </p>
+      </div>
+    </>
+  );
+}

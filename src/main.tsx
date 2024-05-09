719ac7e61553a113b76a6d1ca3459dc001b108b3
Abdulbasit Yusuf <jideotetic@gmail.com>
2024-05-09 17:56:58 +0000

    Removed loaders from each route

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,6 @@ import forgotPasswordAction from "./actions/forgotPasswordAction";
 import verifyOTPAction from "./actions/verifyOTPAction";
 import createNewPasswordAction from "./actions/createNewPasswordAction";
 import loginAction from "./actions/loginAction";
-import rootLoader from "./loaders/rootLoader";
 import ErrorPage from "./ErrorPage";
 import Spinner from "./components/Spinner";
 import GlobalErrorPage from "./GlobalErrorPage";
@@ -22,42 +21,35 @@ const router = createBrowserRouter([
     path: "/",
     id: "root",
     element: <Root />,
-    // loader: rootLoader,
     errorElement: <GlobalErrorPage />,
     children: [
       {
         index: true,
         element: <Login />,
-        loader: rootLoader,
         action: loginAction,
         errorElement: <ErrorPage />,
       },
       {
         path: "forgot-password",
         element: <ForgotPassword />,
-        loader: rootLoader,
         action: forgotPasswordAction,
         errorElement: <ErrorPage />,
       },
       {
         path: "verify-otp",
         element: <VerifyOTP />,
-        loader: rootLoader,
         action: verifyOTPAction,
         errorElement: <ErrorPage />,
       },
       {
         path: "create-new-password",
         element: <CreateNewPassword />,
-        loader: rootLoader,
         action: createNewPasswordAction,
         errorElement: <ErrorPage />,
       },
       {
         path: "dashboard",
-        id: "dashboard",
         element: <Dashboard />,
-        loader: rootLoader,
       },
     ],
   },
